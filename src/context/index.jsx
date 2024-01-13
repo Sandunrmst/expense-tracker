72126fef213163089fa50179e11aee6d18f899b5
sandunrmst <sandunalugc@gmail.com>
2024-01-13 18:24:00 +0530

    Handle Form Data

--- src/context/index.jsx
+++ src/context/index.jsx
@@ -1,7 +1,49 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
-export const GlobalContext = createContext(null)
+export const GlobalContext = createContext(null);
 
-export default function GlobalState({children}){
-    return <GlobalContext.Provider>{children}</GlobalContext.Provider>
+export default function GlobalState({ children }) {
+  const [formData, setFormData] = useState({
+    type: "expense",
+    amount: 0,
+    description: "",
+  });
+
+  const [value, setValue] = useState("expense");
+  const [totalExpense, setTotalExpense] = useState(0);
+  const [totalIncome, setTotalIncome] = useState(0);
+  const [allTransaction, setAllTransaction] = useState([]);
+
+  function handleFormSubmit(currentFormData) {
+    console.log(currentFormData);
+    //Check the courrent form data is empty
+    if (!currentFormData.description || !currentFormData.amount) return;
+
+    setAllTransaction([
+      ...allTransaction,
+      { ...currentFormData, id: Date.now() },
+    ]);
+  }
+
+  console.log(allTransaction);
+
+  return (
+    <GlobalContext.Provider
+      value={{
+        formData,
+        setFormData,
+        totalExpense,
+        setTotalExpense,
+        totalIncome,
+        setTotalIncome,
+        value,
+        setValue,
+        allTransaction,
+        setAllTransaction,
+        handleFormSubmit,
+      }}
+    >
+      {children}
+    </GlobalContext.Provider>
+  );
 }
